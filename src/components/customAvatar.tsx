77c38c1b4fcb78edfdfe6688afdd0cea33689339
Frank Wang <frankwaang@outlook.com>
2024-03-28 20:27:26 -0500

    chore: ...

--- src/components/customAvatar.tsx
+++ src/components/customAvatar.tsx
@@ -17,25 +17,31 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "./ui/input";
 
-function JoinRoomDialog({onClick}: { onClick: (roomId: string) => void}) {
+function JoinRoomDialog({ onClick }: { onClick: (roomId: string) => void }) {
   const [roomId, setRoomId] = useState("");
   return (
-    <Dialog >
+    <Dialog>
       <DialogTrigger asChild>
-      <Button
-        className="ring-offset-3 flex h-[80px] w-[200px] items-center justify-center rounded-xl text-[32px] italic ring-8 ring-orange-600 ring-offset-black transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-[#f7d726]"
-      >
-        Join!
-      </Button>
+        <Button className="ring-offset-3 flex h-[80px] w-[200px] items-center justify-center rounded-xl text-[32px] italic ring-8 ring-orange-600 ring-offset-black transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-[#f7d726]">
+          Join!
+        </Button>
       </DialogTrigger>
       <DialogContent className="bg-secondary justify-center flex flex-col">
         <DialogHeader>
           <DialogTitle className="font-sans text-white">
-            Enter The Room Id to Join 
+            Enter The Room Id to Join
           </DialogTitle>
         </DialogHeader>
-        <Input value={roomId} onInput={(e) => {setRoomId(e.currentTarget.value)}}/>
-        <Button className="rounded-xl italic ring-[5px] ring-orange-600 hover:bg-[#f7d726] text-shadow-md" onClick={() => onClick(roomId)}>
+        <Input
+          value={roomId}
+          onInput={(e) => {
+            setRoomId(e.currentTarget.value);
+          }}
+        />
+        <Button
+          className="rounded-xl italic ring-[5px] ring-orange-600 hover:bg-[#f7d726] text-shadow-md"
+          onClick={() => onClick(roomId)}
+        >
           Submit
         </Button>
       </DialogContent>
@@ -117,89 +123,105 @@ export function AvatarPicker() {
   };
 
   return (
-
-      <div className="w-[1082px] h-[698px] absolute border-4 border-gray-300 shadow-inner rounded-lg left-[50%] top-[50%] -translate-x-1/2 -translate-y-1/2 lg:scale-95 md:scale-90 sm:scale-[0.6]">
-    <div className="flex h-full flex-col items-center justify-center space-y-10">
-      <h1 className="font-customs text-shadow-custom text-[50px] text-[#8DFCBC]">
-        PixeLana
-      </h1>
-      <div className="flex w-full items-center justify-center ">
-        <Button
-          className="ring-none border-none bg-transparent transition ease-in-out hover:-translate-y-1 hover:scale-110"
-          onClick={prev}
-        >
-          {/* <SvgIcon pathData={leftPath} width={size} height={size} viewBox = {`0 0 ${size} ${size}`}/> */}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="120"
-            height="120"
-            viewBox="0 0 256 256"
+    <div className="w-[1082px] h-[698px] absolute border-4 border-gray-300 shadow-inner rounded-lg left-[50%] top-[50%] -translate-x-1/2 -translate-y-1/2 lg:scale-95 md:scale-90 sm:scale-[0.6]">
+      <div className="flex h-full flex-col items-center justify-center space-y-10">
+        <h1 className="font-customs text-shadow-custom text-[50px] text-[#8DFCBC]">
+          PixeLana
+        </h1>
+        <div className="flex w-full items-center justify-center ">
+          <Button
+            className="ring-none border-none bg-transparent transition ease-in-out hover:-translate-y-1 hover:scale-110"
+            onClick={prev}
           >
-            <path
-              stroke={"black"}
-              stroke-width="6"
-              fill="#F9E05A"
-              d="M168 48v160a8 8 0 0 1-13.66 5.66l-80-80a8 8 0 0 1 0-11.32l80-80A8 8 0 0 1 168 48"
-            ></path>
-          </svg>
-        </Button>
-        <Avatar className="bg-primary h-[175px] w-[175px] rounded-full border-[5px] border-black">
-          <AvatarImage src={`/avatars/${avatars[chosenIndex]}`} alt="avatar" />
-        </Avatar>
-        {/* <SvgIcon pathData={avatarArray[chosenIndex]} width={"100"} height={"100"} /> */}
-        <Button
-          onClick={prev}
-          className="ring-none border-none bg-transparent transition ease-in-out hover:-translate-y-1 hover:scale-110"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="120"
-            height="120"
-            viewBox="0 0 256 256"
+            {/* <SvgIcon pathData={leftPath} width={size} height={size} viewBox = {`0 0 ${size} ${size}`}/> */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="120"
+              height="120"
+              viewBox="0 0 256 256"
+            >
+              <path
+                stroke={"black"}
+                stroke-width="6"
+                fill="#F9E05A"
+                d="M168 48v160a8 8 0 0 1-13.66 5.66l-80-80a8 8 0 0 1 0-11.32l80-80A8 8 0 0 1 168 48"
+              ></path>
+            </svg>
+          </Button>
+          <Avatar className="bg-primary h-[175px] w-[175px] rounded-full border-[5px] border-black">
+            <AvatarImage
+              src={`/avatars/${avatars[chosenIndex]}`}
+              alt="avatar"
+            />
+          </Avatar>
+          {/* <SvgIcon pathData={avatarArray[chosenIndex]} width={"100"} height={"100"} /> */}
+          <Button
+            onClick={prev}
+            className="ring-none border-none bg-transparent transition ease-in-out hover:-translate-y-1 hover:scale-110"
           >
-            <path
-              stroke={"black"}
-              stroke-width="6"
-              fill="#F9E05A"
-              d="m181.66 133.66l-80 80A8 8 0 0 1 88 208V48a8 8 0 0 1 13.66-5.66l80 80a8 8 0 0 1 0 11.32"
-            ></path>
-          </svg>
-        </Button>
-      </div>
-
-      <div className="bg-primary ring-offset-background relative flex h-[50px] w-[250px] items-center rounded-lg border-[4px] border-black px-3 py-2 text-sm">
-        <div className="flex w-full text-lg">
-          <span className="font-sans ">{name}</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="120"
+              height="120"
+              viewBox="0 0 256 256"
+            >
+              <path
+                stroke={"black"}
+                stroke-width="6"
+                fill="#F9E05A"
+                d="m181.66 133.66l-80 80A8 8 0 0 1 88 208V48a8 8 0 0 1 13.66-5.66l80 80a8 8 0 0 1 0 11.32"
+              ></path>
+            </svg>
+          </Button>
         </div>
-        <div
-          className="absolute right-1"
-          onClick={() => setName(genRandomName())}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="1.5em"
-            height="1.25em"
-            viewBox="0 0 640 512"
+
+        <div className="bg-primary ring-offset-background relative flex h-[50px] w-[250px] items-center rounded-lg border-[4px] border-black px-3 py-2 text-sm">
+          <div className="flex w-full text-lg">
+            <span className="font-sans ">{name}</span>
+          </div>
+          <div
+            className="absolute right-1"
+            onClick={() => setName(genRandomName())}
           >
-            <path
-              fill="currentColor"
-              d="M274.9 34.3c-28.1-28.1-73.7-28.1-101.8 0L34.3 173.1c-28.1 28.1-28.1 73.7 0 101.8l138.8 138.8c28.1 28.1 73.7 28.1 101.8 0l138.8-138.8c28.1-28.1 28.1-73.7 0-101.8zM200 224a24 24 0 1 1 48 0a24 24 0 1 1-48 0M96 200a24 24 0 1 1 0 48a24 24 0 1 1 0-48m128 176a24 24 0 1 1 0-48a24 24 0 1 1 0 48m128-176a24 24 0 1 1 0 48a24 24 0 1 1 0-48m-128-80a24 24 0 1 1 0-48a24 24 0 1 1 0 48m96 328c0 35.3 28.7 64 64 64h192c35.3 0 64-28.7 64-64V256c0-35.3-28.7-64-64-64H461.7c11.6 36 3.1 77-25.4 105.5L320 413.8zm160-120a24 24 0 1 1 0 48a24 24 0 1 1 0-48"
-            ></path>
-          </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1.5em"
+              height="1.25em"
+              viewBox="0 0 640 512"
+            >
+              <path
+                fill="currentColor"
+                d="M274.9 34.3c-28.1-28.1-73.7-28.1-101.8 0L34.3 173.1c-28.1 28.1-28.1 73.7 0 101.8l138.8 138.8c28.1 28.1 73.7 28.1 101.8 0l138.8-138.8c28.1-28.1 28.1-73.7 0-101.8zM200 224a24 24 0 1 1 48 0a24 24 0 1 1-48 0M96 200a24 24 0 1 1 0 48a24 24 0 1 1 0-48m128 176a24 24 0 1 1 0-48a24 24 0 1 1 0 48m128-176a24 24 0 1 1 0 48a24 24 0 1 1 0-48m-128-80a24 24 0 1 1 0-48a24 24 0 1 1 0 48m96 328c0 35.3 28.7 64 64 64h192c35.3 0 64-28.7 64-64V256c0-35.3-28.7-64-64-64H461.7c11.6 36 3.1 77-25.4 105.5L320 413.8zm160-120a24 24 0 1 1 0 48a24 24 0 1 1 0-48"
+              ></path>
+            </svg>
+          </div>
         </div>
-      </div>
-      <div className="items-center justify-center flex flex-row gap-10">
-      <Button
-        className="ring-offset-3 flex h-[80px] w-[200px] items-center justify-center rounded-xl text-[32px] italic ring-8 ring-orange-600 ring-offset-black transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-[#f7d726]"
-        onClick={() => connectSocket({name:name, avatar:`/avatars/${avatars[chosenIndex]}`, pubKey: wallet.publicKey?.toBase58()!})}
-      >
-        New Game!
-      </Button>
-      <JoinRoomDialog onClick={(roomId) => {
-        // setGameId(roomId);
-        connectSocket({name:name, avatar:`/avatars/${avatars[chosenIndex]}`, pubKey: wallet.publicKey?.toBase58()!, roomId})}} />
-      </div>
-      {/* <Button
+        <div className="items-center justify-center flex flex-row gap-10">
+          <Button
+            className="ring-offset-3 flex h-[80px] w-[200px] items-center justify-center rounded-xl text-[32px] italic ring-8 ring-orange-600 ring-offset-black transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-[#f7d726]"
+            onClick={() =>
+              connectSocket({
+                name: name,
+                avatar: `/avatars/${avatars[chosenIndex]}`,
+                pubKey: wallet.publicKey?.toBase58()!,
+              })
+            }
+          >
+            New Game!
+          </Button>
+          <JoinRoomDialog
+            onClick={(roomId) => {
+              // setGameId(roomId);
+              connectSocket({
+                name: name,
+                avatar: `/avatars/${avatars[chosenIndex]}`,
+                pubKey: wallet.publicKey?.toBase58()!,
+                // roomId,
+              });
+            }}
+          />
+        </div>
+        {/* <Button
         className="ring-offset-3 flex h-[100px] w-[500px] items-center justify-center rounded-xl text-[64px] italic ring-8 ring-orange-600 ring-offset-black transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-[#f7d726]"
         onClick={() => connectSocket({name:name, avatar:`/avatars/${avatars[chosenIndex]}`, pubKey: wallet.publicKey?.toBase58()!})}
       >
@@ -222,7 +244,7 @@ export function AvatarPicker() {
         </div>
         Start!
       </Button> */}
+      </div>
     </div>
-</div>
   );
 }
